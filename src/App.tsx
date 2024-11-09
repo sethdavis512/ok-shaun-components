bd90e5164c98011c5d80dff89bd25cd58d2674fc
Shaun Fox <shaunrfox@gmail.com>
2024-11-09 08:52:38 -0600

    Added storybook and playroom

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import styled from '@emotion/styled';
-import Button from './components/Button';
-import Card from './components/Card';
+import styled from "@emotion/styled";
+import Button from "./components/Button";
+import Card from "./components/Card";
 
 const Flex = styled.div`
-    display: flex;
-    gap: 1rem;
+  display: flex;
+  gap: 1rem;
 `;
 
 export default function App() {
-    return (
-        <>
-            <h1>OK Shaun Componentz</h1>
-            <Flex>
-                {Array(3)
-                    .fill(null)
-                    .map(() => (
-                        <Card>
-                            <Button>Click this!</Button>
-                        </Card>
-                    ))}
-            </Flex>
-        </>
-    );
+  return (
+    <>
+      <h1>OK Shaun Componentz</h1>
+      <Flex>
+        {Array(3)
+          .fill(null)
+          .map(() => (
+            <Card key={Math.random()}>
+              <Button>Click this!</Button>
+            </Card>
+          ))}
+      </Flex>
+    </>
+  );
 }
